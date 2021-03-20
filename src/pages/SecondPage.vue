64ac220c8331cbfb19e600b8aa208dc95ed315a3
Mikhail <rybkamikhail1996@gmail.com>
2021-03-21 01:23:11 +0300

    Added output of code and effect name for all pages

--- src/pages/SecondPage.vue
+++ src/pages/SecondPage.vue
@@ -1,6 +1,14 @@
 <template>
 	<div id="page">
 		<h4>SECOND PAGE</h4>
+		<div v-if="$effectPage.current" id="current-effect-info">
+			<div>
+				<b>NAME: </b> <span>{{ $effectPage.current.name }}</span>
+			</div>
+			<div>
+				<b>CODE: </b> <span>{{ $effectPage.current.code }}</span>
+			</div>
+		</div>
 	</div>
 </template>
 
